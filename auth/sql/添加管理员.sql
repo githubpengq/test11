08577f76fdb82c44b5b711ab7c57764d84197544
testname <testname@163.com>
2018-01-25 16:29:38 +0800

    对方答复

--- auth/sql/添加管理员.sql
+++
@@ -1,6 +0,0 @@
-﻿添加管理员步骤：（注：手机号不能重复，密码为123456）
-
-1、sql语句示例：INSERT INTO `t_user`(`version`,`duties`,`mobile`,`name`,`password`,`role`) VALUES (0,'客服', '15381179572', '李东兰', '14e1b600b1fd579f47433b88e8d85291', '管理人员');
-2、添加对应的mobile(手机号)、name(姓名)：
-3、执行sql
-4、测试的语句
