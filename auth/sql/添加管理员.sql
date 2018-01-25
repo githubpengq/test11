3cea4f205b40f50f8e8f7b9308f15345006ce835
testname <testname@163.com>
2018-01-25 16:25:51 +0800

    测试版本

--- auth/sql/添加管理员.sql
+++ auth/sql/添加管理员.sql
@@ -3,3 +3,4 @@
 1、sql语句示例：INSERT INTO `t_user`(`version`,`duties`,`mobile`,`name`,`password`,`role`) VALUES (0,'客服', '15381179572', '李东兰', '14e1b600b1fd579f47433b88e8d85291', '管理人员');
 2、添加对应的mobile(手机号)、name(姓名)：
 3、执行sql
+4、测试的语句
